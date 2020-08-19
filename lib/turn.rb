4f230bf0a20d1578eb9563c18af2924812398f3d
grateful-closure-7122 <alex@artofalexwaller.com>
2020-08-19 07:08:01 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,7 +14,7 @@ end
 
 def valid_move?(board,index)
   def position_taken?(array,ind)
-    if array[ind]== " " || array[ind]== "" || array[ind]== nil
+    if array[ind]== " " || array[ind]== "" || array[ind] == nil
       return false
     else
       return true
